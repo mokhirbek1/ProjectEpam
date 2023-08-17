20ff41b4ee68cd839b58250cc6d136d0c755b59c
Moxirbek <moxirbek021199@gmail.com>
2023-08-17 09:29:08 +0500

    Nimadur o'zgardi

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,5 +3,11 @@ package com.example.demo11;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bir balo");
+        System.out.println("Bir balo");
+        System.out.println("Bir balo");
+        System.out.println("Bir balo");
+        System.out.println("Bir balo");
+        System.out.println("Bir balo");
+        System.out.println("Bir balo");
     }
 }
